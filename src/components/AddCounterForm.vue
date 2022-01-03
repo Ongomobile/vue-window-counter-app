1b1568b48485fb89e027179fa5b5113f73237450
ongomobile <ongomobile@gmail.com>
2022-01-03 14:57:16 -0800

    Add header & add counter form & global styles

---
+++ src/components/AddCounterForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="add-counter-form-wrapper">
+    <h1>Add Form Here</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header-wrapper">
+    <h1 class="header">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+.header-wrapper {
+  padding-top: 3em;
+  display: flex;
+  justify-content: center;
+}
+.header {
+  text-transform: capitalize;
+}
+</style>
