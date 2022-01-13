5c3eb119b50ac8922fd7e615b41b32bf7ed60bb0
ongomobile <ongomobile@gmail.com>
2022-01-13 13:54:44 -0800

    Add google gtag

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(fas)
+import VueGtag from 'vue-gtag'
 
 const windowCounterApp = createApp(App)
+windowCounterApp.use(VueGtag, {
+  config: { id: 'G-9K2MVBLC30' }
+})
 windowCounterApp.component('fa', FontAwesomeIcon)
 windowCounterApp.mount('#app')
